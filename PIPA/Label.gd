3ce98478e11205e5862a451e9a208a0fe787b690
pajilayajepa22 <47749898+pajilayajepa22@users.noreply.github.com>
2019-02-18 17:20:16 +0200

    Add files via upload

---
+++ PIPA/Label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var number = 0
+# Called when the node enters the scene tree for the first time.
+func _ready()
+func _ready():
+	set_text("Hello World!") # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
